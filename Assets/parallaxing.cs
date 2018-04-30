7b81666c84ec8b05d1c279b7b2161afaccdce865
CG-R8 <cgajare1@binghamton.edu>
2018-04-30 13:31:05 -0400

    Unity 2D Game
    Build at version 5

---
+++ Assets/parallaxing.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxing : MonoBehaviour {
+
+    public Transform[] backgrounds;
+    private float[] parallaxScale;
+    public float smoothing =1;         //How smooth the parallax gonna be.
+    private Transform cam;
+    private Vector3 previousCamPosition;
+
+    //should be called before Start.just to get the references  
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Use this for initialization
+    void Start () {
+        previousCamPosition = cam.position;
+        parallaxScale = new float[backgrounds.Length];
+        for(int i=0; i < backgrounds.Length; i++)
+        {
+            parallaxScale[i] = backgrounds[i].position.z * -1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPosition.x - cam.position.x) * parallaxScale[i];
+
+            //set a target X position which is the current position + parallax
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+            //create a taerget position which is the background current position with its target X position.
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            //fade
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+
+        }
+        previousCamPosition = cam.position;
+    }
+}
---
+++ Assets/tilling.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))] 
+
+public class tilling : MonoBehaviour {
+    public int offsetX = 2;             //Offset that we dont get any errors 
+
+    //To check if we need the instantiate stuff.
+    public bool hasARightBuddy = false;
+    public bool hasALeftBuddy = false;
+
+    public bool reverseScale = false;       
+    private float spriteWidth = 0;
+
+    private Camera cam;
+    private Transform myTransform;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        myTransform = transform;
+    }
+    // Use this for initialization
+    void Start () {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();  //WIll return 1st one
+        spriteWidth = sRenderer.sprite.bounds.size.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(hasALeftBuddy==false || hasARightBuddy == false)
+        {
+            //Calculate tthe camara extend (half of the width ) of what camara can see
+            float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+
+            //calculate the x position where camara can see the edge of the sprite
+            float edgeVisiblePositionRight = (myTransform.position.x + spriteWidth / 2) - camHorizontalExtend;
+            float edgeVisiblePositionLeft = (myTransform.position.x - spriteWidth / 2) + camHorizontalExtend;
+
+
+            //Checking if we can see the element. and then calling make new buddy.
+            if(cam.transform.position.x >= edgeVisiblePositionRight - offsetX && hasARightBuddy == false)
+            {
+                MakenewBuddy(1);
+                hasARightBuddy = true;
+            }
+
+            else if (cam.transform.position.x >= edgeVisiblePositionLeft + offsetX && hasALeftBuddy == false)
+            {
+                MakenewBuddy(-1);
+                hasALeftBuddy = true;
+            }
+
+        }
+	}
+
+    void MakenewBuddy(int rightOrLeft)
+    {
+        //calculating hte new position for new buddyd 
+        Vector3 newPosition = new Vector3 (myTransform.position.x + spriteWidth * rightOrLeft,myTransform.position.y,myTransform.position.z);
+        Transform newBuddy = Instantiate(myTransform,newPosition,myTransform.rotation) as Transform;
+
+        //if not tilable let's reverse it 
+        if(reverseScale == true)
+        {
+            newBuddy.localScale = new Vector3(newBuddy.localScale.x * -1, newBuddy.localScale.y, newBuddy.localScale.z);
+        }
+
+        newBuddy.parent = myTransform.parent;
+        if(rightOrLeft > 0)
+        {
+            newBuddy.GetComponent<tilling>().hasALeftBuddy = true;
+        }
+        else
+        {
+            newBuddy.GetComponent<tilling>().hasARightBuddy = true;
+        }
+
+    }
+}
